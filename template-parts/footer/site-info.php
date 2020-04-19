320e9b0c7308e5064584686edde243ff286151ca
Dan Shedd <dan@dshedd.com>
2020-04-19 18:27:40 -0400

    Link to new tab in footer

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -6,7 +6,7 @@
 ?>
 <div class="site-info row m-auto">
 	
-	<div class="col-lg-4 mb-2 mb-lg-0">Proudly made with <a href="https://wordpress.org/">WordPress</a></div> 
+	<div class="col-lg-4 mb-2 mb-lg-0">Proudly made with <a href="https://wordpress.org/" target="_blank">WordPress</a></div> 
 	<div class="copyright col-lg-4 mb-2 mb-lg-0"><?php esc_html_e( get_bloginfo( 'name' ) . ' &copy;' . date('Y') ); ?></div>
 	<div class="col-lg-4 mb-2 mb-lg-0">Made with <span aria-hidden="true"><span class="screen-reader-text">heart</span>&hearts;</span> in Lowell, MA</div>
 
