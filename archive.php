a020750e546c7ffe7ef9dcb452d40f0bc9f94fe6
Dan Shedd <dan@dshedd.com>
2020-05-03 13:57:47 -0400

    Generalize archive template

--- archive.php
+++ archive.php
@@ -2,20 +2,27 @@
 
 	get_header(); 
 
-	$the_term = get_queried_object();
+	$queried_object = get_queried_object();
+
+	if( is_category() ) {
+		$page_title = 'Blog Category: ' . $queried_object->name;
+	} elseif( is_post_type_archive() ) {
+		$labels = get_post_type_labels( $queried_object );
+		$page_title = $labels->name;
+	}
 ?>
 
 <div class="wrap container">
 
 	<?php if ( have_posts() ) : ?>
 		<header class="page-header mb-5">
-			<h1 class="page-title"><?php esc_html_e( $the_term->name ); ?></h1>
+			<h1 class="page-title"><?php esc_html_e( $page_title ); ?></h1>
 		</header><!-- .page-header -->
 	<?php endif; ?>
 
 	<div id="primary" class="content-area row">
 
-		<?php get_template_part( 'template-parts/post/content', 'archive-loop' ); ?>
+		<?php get_template_part( 'template-parts/' . get_post_type() . '/content', 'archive-loop' ); ?>
 
 	</div><!-- #primary -->
 	
